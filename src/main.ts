706dd47113675fd8eb847f869be6218090b1d01a
Wenxuan <gwx2013@mail.ustc.edu.cn>
2019-03-18 03:03:17 -0700

    name

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { startWeb }   from './start-web'
 async function main () {
   log.verbose('main', 'main()')
 
-  const name = process.env.WECHATY_NAME || 'heroku-wechaty'
+  const name = process.env.WECHATY_NAME || 'djudju-bot'
 
   const bot = getWechaty(name)
 
