876032310fdf4d503b4792ab2cc81e87aa58d707
Wenxuan <gwx2013@mail.ustc.edu.cn>
2019-03-18 02:41:39 -0700

    config

--- src/handlers/on-message.ts
+++ src/handlers/on-message.ts
@@ -9,4 +9,16 @@ export async function onMessage (
   message : Message,
 ): Promise<void> {
   log.info('on-message', 'onMessage(%s)', message)
+  const text = message.text
+  if (msg.self()) {
+    return
+  }
+
+
+  if (/dju/.test(text)) {
+    message.say('djudju爱你哦!')
+    message.say("http://i1.bvimg.com/680813/1b2a8f97f2db09d7.jpg")
+  }
+
+
 }
