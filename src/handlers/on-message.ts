56b0e1a7057d3ffe99196fb683013629ccbf1400
Wenxuan <gwx2013@mail.ustc.edu.cn>
2019-03-18 02:44:01 -0700

    lint

--- src/handlers/on-message.ts
+++ src/handlers/on-message.ts
@@ -9,15 +9,15 @@ export async function onMessage (
   message : Message,
 ): Promise<void> {
   log.info('on-message', 'onMessage(%s)', message)
-  const text = message.text
-  if (msg.self()) {
+  const text = message.text()
+
+  if (message.self()) {
     return
   }
 
-
   if (/dju/.test(text)) {
     message.say('djudju爱你哦!')
-    message.say("http://i1.bvimg.com/680813/1b2a8f97f2db09d7.jpg")
+    message.say('http://i1.bvimg.com/680813/1b2a8f97f2db09d7.jpg')
   }
 
 
